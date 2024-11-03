524813ccafc0b82528842646a40a00b11fc3d8a7
ali <63862994+nessed@users.noreply.github.com>
2024-11-04 02:37:03 +0500

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+void add(int num1, int num2){
+  int sum = num1+num2;
+  cout<<sum<<endl;
+
+}
+
+int main() {
+  int num1 = 2;
+  int num2 = 2;
+  add(num1,num2);
+  cout<<"hi";
+  
+return 0; 
+}
+
+
