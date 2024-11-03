966ad933d39607fdacb4dd3d5f8515b09461b07e
ali <63862994+nessed@users.noreply.github.com>
2024-11-04 02:24:51 +0500

    Create happyBirthday.cpp

---
+++ happyBirthday.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+
+void happyBirthday(string inputName, int years){
+  cout<<"Happy birthday to "<<inputName<<endl;
+  cout<<"Happy birthday to "<<inputName<<endl;
+  cout<<"Happy birthday your turning "<<years<<endl;
+}
+
+int main() {
+  int age = 17;
+  string name = "Ali";
+  happyBirthday(name,age);
+ 
+  cout<<"hi";
+  
+return 0; 
+}
+
+
